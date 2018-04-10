37d6802be40045b177abe2797292b5cf4fafd509
Pulkit Gupta <gupta.pulkit91@gmail.com>
2018-04-10 13:58:33 +0900

    Fix requirements issue, add nosetests

--- setup.py
+++ setup.py
@@ -1,27 +1,83 @@
 # coding: utf-8
+import ast
+import io
 import os
+import re
 from setuptools import setup
-import hitbtcapi
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
-REQUIREMENTS = [line.strip() for line in open(os.path.join(os.path.dirname(__file__),'requirements.txt')).readlines()]
+class VersionFinder(ast.NodeVisitor):
+    def __init__(self):
+        self.version = None
+
+    def visit_Assign(self, node):
+        if node.targets[0].id == '__version__':
+            self.version = node.value.s
+
+
+def read_version():
+    """Read version without loading any files"""
+    finder = VersionFinder()
+    finder.visit(
+        ast.parse(local_file('hitbtcapi', '__init__.py').encode('utf-8')))
+    return finder.version
+
+
+def parse_requirements(path):
+    """Rudimentary parser for the `requirements.txt` file
+    We just want to separate regular packages from links to pass them to the
+    `install_requires` and `dependency_links` params of the `setup()`
+    function properly.
+    """
+    try:
+        requirements = [req.strip() for req in local_file(path).splitlines()]
+    except IOError:
+        raise RuntimeError("Could not find the `requirements.txt' file")
+
+    links = []
+    pkgs = []
+    for req in requirements:
+        if not req:
+            continue
+        if 'http:' in req or 'https:' in req:
+            links.append(req)
+            name, version = re.findall("\#egg=([^\-]+)-(.+$)", req)[0]
+            pkgs.append('{}=={}'.format(name, version))
+        else:
+            pkgs.append(req)
+
+    return pkgs, links
+
+
+local_file = lambda *f: \
+    io.open(
+        os.path.join(os.path.dirname(__file__), *f), encoding='utf-8').read()
+
+
+install_requires, dependency_links = \
+    parse_requirements('requirements.txt')
+tests_requires, tests_dependency_links = \
+    parse_requirements('test-requirements.txt')
+
+
 
 setup(
     name='hitbtcapi',
-    version=hitbtcapi.__version__,
+    version=read_version(),
     packages=['hitbtcapi'],
-    include_package_data=True,
-    license='MIT',
     description='HitBTC API Client library',
-    long_description=README,
-    url='https://github.com/pulkit1991/hitbtcapi/',
-    download_url='https://github.com/pulkit1991/hitbtcapi/tarball/%s' % (
-      hitbtcapi.__version__),
-    keywords=['hitbtc', 'api', 'client', 'bitcoin', 'altcoin', 'trading'],
-    install_requires=REQUIREMENTS,
+    long_description=local_file('README.rst'),
     author='Pulkit Gupta',
     author_email='gupta.pulkit91@gmail.com',
+    url='https://github.com/pulkit1991/hitbtcapi',
+    download_url='https://github.com/pulkit1991/hitbtcapi/tarball/%s' % read_version(),
+    keywords=['hitbtc', 'api', 'client', 'bitcoin', 'altcoin', 'trading'],
+    include_package_data=True,
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    test_suite='nose.collector',
+    tests_require=tests_requires,
+    license='MIT',
     classifiers=[
           'Intended Audience :: Developers',
           'License :: OSI Approved :: MIT License',
